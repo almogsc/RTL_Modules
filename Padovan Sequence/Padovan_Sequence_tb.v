007f2832da09ae359cdc3162d66631ebcc8d5954
almogsc <76109073+almogsc@users.noreply.github.com>
2023-02-04 11:14:47 +0200

    Create Padovan_Sequence_tb.v

---
+++ Padovan Sequence/Padovan_Sequence_tb.v
@@ -0,0 +1,20 @@
+module Padovan_Sequence_tb();
+    
+    wire [15:0]f_n, n;
+    wire valid;
+    reg clk, rst;
+    Padovan_Sequence dut(.clk(clk), .rst(rst), .f_n(f_n), .n(n), .valid(valid));
+    
+    initial
+    begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+    
+    initial
+    begin
+        rst = 1;
+        #5 rst = 0;
+        #5 rst = 1;
+    end
+endmodule
